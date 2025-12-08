156382c4a4afc35844419689edaa37af6d94a9c6
Eric Riveiro <eric.riveiro@estudiants.urv.cat>
2025-12-08 12:36:39 +0100

    feat: Implementar validaciones de datos en entidad Proveedor - Añadir restricción de unicidad (UniqueEntity) para evitar emails duplicados. - Validar formato numérico y longitud mínima/máxima en el teléfono. - Asegurar formato de email válido y campos obligatorios.

--- src/Entity/Proveedor.php
+++ src/Entity/Proveedor.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use App\Repository\ProveedorRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ProveedorRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'Ya existe un proveedor con este email.')]
 class Proveedor
 {
     #[ORM\Id]
@@ -17,9 +20,22 @@ class Proveedor
     private ?string $nombre = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'El email no puede estar vacío.')]
+    #[Assert\Email(message: 'El formato del correo {{ value }} no es válido.')] // Valida que tenga @ y punto
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'El teléfono es obligatorio.')]
+    #[Assert\Regex(
+        pattern: '/^\d+$/',
+        message: 'El teléfono solo puede contener números.'
+    )] // Valida que solo haya dígitos del 0 al 9
+    #[Assert\Length(
+        min: 9, 
+        max: 15, 
+        minMessage: 'El teléfono debe tener al menos 9 dígitos',
+        maxMessage: 'El teléfono es demasiado largo'
+    )]
     private ?string $telefono = null;
 
     #[ORM\Column(length: 50)]
